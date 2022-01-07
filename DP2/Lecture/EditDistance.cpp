68a8c3d8ddd227ffca9772b9ac504c0fabf77c44
Sambhav Jain <sambhav20jain.sj@gmail.com>
2022-01-07 08:31:29 +0530

    Create EditDistance.cpp

---
+++ DP2/Lecture/EditDistance.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int edit(string s1, string s2, int** dp){
+    for(int i = 0; i <= s1.length(); i++){
+        dp[i][0] = i;
+    }
+    for(int j = 0; j <= s2.length(); j++){
+        dp[0][j] = j;
+    }
+    for(int i = 1; i <= s1.length(); i++){
+        for(int j = 1; j <= s2.length(); j++){
+            if(s1[s1.length()-i] == s2[s2.length()-j]){
+                dp[i][j] = dp[i-1][j-1];//min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
+            }else{
+                dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
+            }
+        }
+    }
+    return dp[s1.length()][s2.length()];
+}
+
+int main(){
+    // write your code here
+    int t;
+    cin>>t;
+    while(t--){
+        string s1;
+        string s2;
+        cin>>s1;
+        cin>>s2;
+        int** dp = new int*[s1.length()+1];
+        for(int i = 0; i <= s1.length(); i++){
+            dp[i] = new int[s2.length()+1];
+        }
+        cout<<edit(s1,s2,dp)<<endl;
+    }
+    return 0;
+}
